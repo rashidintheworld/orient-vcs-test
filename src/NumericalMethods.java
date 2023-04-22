4a4a14fa96fb258f3974f1ed1f02f743d5ffe010
Rashid Teymurlu <rasidtt999@gmail.com>
2023-04-23 02:49:05 +0400

    Omar added polindrom method to NumericalMethod.java

--- src/NumericalMethods.java
+++ src/NumericalMethods.java
@@ -10,5 +10,23 @@ public class NumericalMethods {
         }
         return true;
     }
+    public static void isPolindrom(int number){
+        int reversedNumber = 0, remainder, originalNumber;
+        originalNumber = number;
 
+        // Tersini tapaq
+        while (number != 0) {
+            remainder = number % 10;
+            reversedNumber = reversedNumber * 10 + remainder;
+            number /= 10;
+        }
+
+        // Polindrom kontrol
+        if (originalNumber == reversedNumber) {
+            System.out.println(originalNumber + " pol-dir");
+        } else {
+            System.out.println(originalNumber + " pol-deyil");
+        }
+    }
+    }
 }
