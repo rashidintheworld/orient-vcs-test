fd9fa5f26b62074ccb843c2e73d5cd8b96f9b37b
Rashid Teymurlu <rasidtt999@gmail.com>
2023-04-23 02:39:30 +0400

    added new changes

---
+++ src/NumericalMethods.java
@@ -0,0 +1,14 @@
+public class NumericalMethods {
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
